b03e6e0fc856011d803266be6ed1895642859d8a
sudeepsubedi99 <50973728+sudeepsubedi99@users.noreply.github.com>
2021-11-14 01:41:47 +0545

    code to brain storm

---
+++ src/com/company/Main.java
@@ -0,0 +1,121 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+//        int a[]={33,3,4,5};//declaration, instantiation and initialization
+////printing array
+//        for(int i=0;i<a.length;i++)//length is the property of array
+//            System.out.println(a[i]);
+
+//        int fooFor;
+//        for ( fooFor = 0; fooFor < 10; fooFor++) {
+//            System.out.println(fooFor);
+//            // Iterated 10 times, fooFor 0->9
+//        }
+//        System.out.println("fooFor Value: " + fooFor);
+
+
+//        System.out.println("enter number");
+//        Scanner naya = new Scanner(System.in);
+//        int i = naya.nextInt();
+//        System.out.println("enter another ");
+//        Scanner nayaY = new Scanner(System.in);
+//        int j = nayaY.nextInt();
+//        outer:
+//        for ( i ; i < 10; i++) {
+//            for ( j ; j < 10; j++) {
+//                if (i == 5 && j ==5) {
+//                    System.out.println("you are right");
+//                    break outer;
+//                    // breaks out of outer loop instead of only the inner one
+//                }
+//            }
+//        }
+
+//        int a[] = new int[5];
+//        a[0] = 10;
+//        a[1] = 20;
+//        a[2] = 30;
+//        a[3] = 40;
+//        a[4] =50;
+//
+//        for (int i=0 ; i<a.length;i++){
+//            System.out.println(a[i]);
+//        }
+
+
+        //swap number
+//   int a, b ,c;
+//   Scanner scan = new Scanner(System.in);
+//        System.out.println("enter a number::a->");
+//        a = scan.nextInt();
+//        System.out.println("enter another number::b->");
+//        b = scan.nextInt();
+//        c=a;
+//        a=b;
+//        b=c;
+//        System.out.println("a="+a);
+//        System.out.println("b="+b);
+
+        // sum of two Number
+
+//        int a, b;
+//        Scanner s = new Scanner(System.in);
+//        System.out.println("enter first number");
+//        a = s.nextInt();
+//        System.out.println("enter number to add");
+//        b = s.nextInt();
+//        System.out.println("Sum = " + (a+b));
+
+        //swap two number without using third value
+//        int a, b;
+//        Scanner s = new Scanner(System.in);
+//        System.out.println("enter a value");
+//        a = s.nextInt();
+//        System.out.println("enter b value");
+//        b = s.nextInt();
+//        a= a + b;
+//        b= a - b;
+//        a= a - b;
+//        System.out.println("value of a = "+a);
+//        System.out.println("value of b = " +b);
+
+        //prime number
+
+//        int n, i=2;
+//        Scanner s = new Scanner(System.in);
+//        System.out.println("please enter a number");
+//        n= s.nextInt();
+//
+//        while (true)
+//        {
+//            if (n==1){
+//                System.out.println("smallest prime number is 2");
+//                break;
+//            }
+//            if (n%i==0){
+//                break;
+//            }else {
+//                i++;
+//            }
+//        }
+//        if (n==i){
+//            System.out.println("Prime no.");
+//        }else {
+//            System.out.println("Composite no.");
+//        }
+
+        //Java program to multiply N numbers without using * operator
+
+
+
+
+
+
+    }
+}
